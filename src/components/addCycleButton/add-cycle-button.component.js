2638d341d53b92ec83b9c097bbda55065d105731
Sal Aslam <aslam.salmam@gmail.com>
2021-10-16 13:13:05 -0400

    started button creation to add a cycle

---
+++ src/components/addCycleButton/add-cycle-button.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Button } from "react-native-paper";
+
+export const AddCycleButton = () => {
+    return (
+        <Button icon="camera" mode="contained">
+        </Button>
+    )
+}
--- src/features/activeCycles/screens/active-cycles.screen.js
+++ src/features/activeCycles/screens/active-cycles.screen.js
@@ -1,16 +1,24 @@
 import React from "react";
 
+import styled from "styled-components/native";
+
 import { SafeArea } from "../../../components/utility/safe-area.component";
-
 import { ActiveCycleCard } from "../components/active-cycle-card.component";
+import {AddCycleButton} from "../../../components/addCycleButton/add-cycle-button.component"
 
-import {billing_data} from "../../../../testing/proxy_data/billing-data.proxy.js";
+import { billing_data } from "../../../../testing/proxy_data/billing-data.proxy.js";
+
+const AddCycle = styled(AddCycleButton)`
+  position: absolute;
+  bottom: 50%;
+`
 
 export const ActiveCycles = () => {
-  console.log(billing_data)
+
   return (
     <SafeArea>
-      {billing_data.map((item) => {return <ActiveCycleCard cycle={item}/>})}
+      {billing_data.map((item, i) => {return <ActiveCycleCard key={i} cycle={item}/>})}
+      <AddCycle />
     </SafeArea>
   );
 };
