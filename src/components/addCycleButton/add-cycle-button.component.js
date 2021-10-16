fb150bc53e140b17f7839890e8eb359f5900db18
Sal Aslam <aslam.salmam@gmail.com>
2021-10-16 13:46:40 -0400

    button created, need styles

--- src/components/addCycleButton/add-cycle-button.component.js
+++ src/components/addCycleButton/add-cycle-button.component.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { View, TouchableOpacity} from "react-native";
+import styled from "styled-components";
 
-import { Button } from "react-native-paper";
+import { Text } from "../typography/text.component";
+import { Entypo } from '@expo/vector-icons';
 
-export const AddCycleButton = () => {
+const Button = styled(TouchableOpacity)`
+    width: 80px;
+    height: 80px;
+    border-radius: 100px;
+    background-color: #138000;
+    justify-content: center;
+    align-items: center;
+`
+
+export const AddCycleButton = ({onPress}) => {
     return (
-        <Button icon="camera" mode="contained">
+        <Button onPress={onPress}>
+            <Entypo name="plus" size={24} color="black" size={50} color="grey"/>
         </Button>
     )
 }
--- src/features/activeCycles/screens/active-cycles.screen.js
+++ src/features/activeCycles/screens/active-cycles.screen.js
@@ -4,21 +4,32 @@ import styled from "styled-components/native";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { ActiveCycleCard } from "../components/active-cycle-card.component";
-import {AddCycleButton} from "../../../components/addCycleButton/add-cycle-button.component"
+import { AddCycleButton } from "../../../components/addCycleButton/add-cycle-button.component";
 
 import { billing_data } from "../../../../testing/proxy_data/billing-data.proxy.js";
 
-const AddCycle = styled(AddCycleButton)`
+
+const AddCycleContainer = styled.View`
   position: absolute;
-  bottom: 50%;
+  bottom: 4%;
+  right: 8%;
 `
 
+
+
 export const ActiveCycles = () => {
+
+  const onAddStyle = () => {
+    console.log("Add Cycle");
+  }
 
   return (
     <SafeArea>
       {billing_data.map((item, i) => {return <ActiveCycleCard key={i} cycle={item}/>})}
-      <AddCycle />
+      <AddCycleContainer>
+        <AddCycleButton onPress={onAddStyle}/>
+      </AddCycleContainer>
+      
     </SafeArea>
   );
 };
