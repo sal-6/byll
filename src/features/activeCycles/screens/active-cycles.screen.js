d5f154835dd0579a13a84c274eb146abd731f8ce
Sal Aslam <aslam.salmam@gmail.com>
2021-10-16 13:01:10 -0400

    imported proxy data

--- src/features/activeCycles/screens/active-cycles.screen.js
+++ src/features/activeCycles/screens/active-cycles.screen.js
@@ -4,12 +4,13 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 
 import { ActiveCycleCard } from "../components/active-cycle-card.component";
 
+import {billing_data} from "../../../../testing/proxy_data/billing-data.proxy.js";
+
 export const ActiveCycles = () => {
+  console.log(billing_data)
   return (
     <SafeArea>
-      <ActiveCycleCard />
-      <ActiveCycleCard />
-      <ActiveCycleCard />
+      {billing_data.map((item) => {return <ActiveCycleCard cycle={item}/>})}
     </SafeArea>
   );
 };
